e42a8c3fd8f74f9a783b7922fc0c7100463e1769
Owen <oweneller@att.net>
2023-09-28 12:14:20 -0400

    week6-vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,143 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
-  <div class="card">
-    <img alt="real estate logo" src="./housepic.jpg">
-    <div class="card-content"> 
-      <h2>Two Pod Realty</h2> 
-      <p id="description">Real Estate Company Focused on delivering the best properties in the State College Area</p> 
-      <button class="details-button" id="detailsBtn">Details</button>
-    </div> 
-  </div> 
   
-  <button id="btn">Duplicator</button>
-  <button id="colorBtn">BGColorChanger</button>
-  <button id="Headingbtn">HeadingChanger</button>
-  <button id="dltbtn">DeleteCard</button>
+    <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="styles.css">
+<title>Card</title>
+
+<div class="wrapper">
+  <div class="container" data-card="1">
+    <div class="card">
+      <div class="card-content">
+        <h2 class="card-title">HAX PSU</h2>
+        <img src="https://hax.camp/assets/haxBanner-01.png" alt="Card Image">
+        <div class="description-toggle">
+          <div class="description">Hax Camp 2022</div>
+          <div class="toggle-button">Toggle Description</div>
+        </div>
+        <a class="details-link" href="https://hax.psu.edu">Details</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<button class="duplicateButton">Duplicate Card</button>
+<button class="toggleColorButton">Toggle Color</button>
+<button class="changeTitleButton">Change Title</button>
+<button class="deleteCardButton">Delete Last Card</button>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  mounted() {
-    document.querySelector('#btn').addEventListener('click', () => {
-      const clone = document.querySelector('.card').cloneNode(true); // Clone the whole card
-      document.body.appendChild(clone);
-    });
-    
-    var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var letters = ["A", "B", "C", "D", "E", "F"];
-    
-    function randomColor() {
-      var color = "";
-      for (var i = 0; i < 3; i++) {
-        var index = Math.floor(Math.random() * (numbers.length + letters.length));
-        color += (index < numbers.length) ? numbers[index] : letters[index - numbers.length];
-      }
-      return color;
-    }
-    
-    document.querySelector('#colorBtn').addEventListener('click', () => {
-      const card = document.querySelector('.card');
-      // Change background color
-      card.style.backgroundColor = '#' + randomColor();
-    });
-    
-    // HEADING CHANGER
-    document.querySelector('#Headingbtn').addEventListener('click', () => {
-      const title = document.querySelector('h2');
-      title.innerText = "Super Pod 2 Realty";
-    });
-    
-    // Card delete
-    document.querySelector('#dltbtn').addEventListener('click', () => {
-      const cards = document.querySelectorAll('.card');
-      // Check if there are cards to delete
-      if (cards.length > 1) {
-        const lastCard = cards[cards.length - 1];
-        lastCard.parentNode.removeChild(lastCard);
-      }
-    });
-    
-    var original = document.querySelector('#description').innerText;
-    
-    document.querySelector('#detailsBtn').addEventListener('click', () => {
-      const description = document.querySelector('#description');
-      // Toggle the visibility of the description
-      if (description.innerText === 'none' || description.innerText === '') {
-        description.innerText = original; // Show the description
-      } else {
-        description.innerText = ''; // Hide the description
-      }
-    });
-  }
-}
-</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-.card {
-  max-width: 400px;
-  margin: 0 auto;
-  border: 1px solid #0C0B0B;
-  background-color: #0C0B0B;
-  padding: 16px;
+body {
   display: flex;
-  flex-direction: column;
-  text-align: center;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  margin: 0;
+  background-color: white;
 }
 
-.card img {
-  max-width: 100%;
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.container {
+  max-width: 400px;
+  border: 4px solid navy;
+  border-radius: 8px;
+  margin: 16px;
+  padding: 16px;
+  background-color: white;
+}
+
+.container img {
+  width: 100%;
   height: auto;
+  border-radius: 10px;
 }
 
 .card-content {
-  flex: 1;
+  margin-top: 0;
+  text-align: center;
 }
 
-/* Style for the Title label */
-h2 {
-  font-size: 24px;
-  margin: -10px 0;
-  /* Background color and border */
-  background-color: #F8471B;
-  /* Background color */
-  color: #fff;
-  /* Text color on the background */
-  padding: 8px 16px;
+.card-title {
+  margin: 16px;
+  text-align: center;
+}
+
+.description {
+  margin: 4px;
+}
+
+.description-toggle {
+  cursor: pointer;
+}
+
+.toggle-button {
+  display: block;
+  text-align: center;
+  padding: 8px;
+  border: 4px solid black;
   border-radius: 4px;
+  margin-top: 16px;
+  color: black;
+  background-color: lightblue;
+}
+.details-link {
+  display: none;
+  margin-top: 16px;
+  text-align: center;
 }
 
-p {
-  font-size: 16px;
-  margin: 8px 0;
-  color: #fff;
+.bg-toggle {
+  background-color: darkgray;
 }
 
-a.details-button {
-  background-color: #EA5F0A;
-  color: #fff;
-  padding: 8px 16px;
-  text-decoration: none;
-  border-radius: 4px;
+.toggle-button:hover,
+.toggle-button:focus {
+  background-color: lightgray;
+  color: black;
+}
+
+.hidden {
+  display: none;
+}
+@media (max-width: 800px) {
+  .button-details {
+    display: none;
+  }
 }
 </style>
